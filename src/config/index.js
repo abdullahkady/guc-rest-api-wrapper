db70052193ea466b40a1ea8dc3ac783a51a706bd
Abdullah Kady <abdullahkady@gmail.com>
2018-11-30 10:08:23 +0200

    Add project configuration

---
+++ src/config/index.js
@@ -0,0 +1,15 @@
+/*
+  This module just initalizes the environment variables.
+  To work with es6 module, check: https://github.com/motdotla/dotenv/issues/133#issuecomment-255298822
+*/
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { PORT, GUC_COURSEWORK_API } = process.env;
+
+const GUC_API_CONFIG = { clientVersion: '1.3', app_os: '0', os_version: '6.0.1' };
+
+export {
+  PORT, GUC_COURSEWORK_API, GUC_API_CONFIG,
+};
